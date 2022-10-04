f869b37a39747cf6f8994ed6d4562d8c96eecc3b
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-10-04 10:37:40 -0300

    chore: add jest configs and scripts

---
+++ jest-unit-config.ts
@@ -0,0 +1,13 @@
+export default {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: true,
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  testMatch: ['**/*.spec.ts']
+}
